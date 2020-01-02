6fca82578536832942a1ffdf764c0e6811ef15bc
Hsinhua <yha0971@gmail.com>
2020-01-02 17:19:08 +0800

    add undo

--- app/admin/posts_admin.rb
+++ app/admin/posts_admin.rb
@@ -23,26 +23,21 @@ Trestle.resource(:posts) do
     text_field :title
     editor :msg
 
-    concat content_tag(:div, nil, class: "panel panel-default"){
-      concat content_tag(:div, '編輯紀錄', class: "panel-heading")
-      concat(content_tag(:div, class: "panel-body") { 
-        content_tag(:table, nil){
-          post.versions.each_with_index do |version, index|
-            concat content_tag(:tr, nil){
-              concat content_tag(:td, index + 1)
-              concat content_tag(:td, nil){
-                concat content_tag(:ul, nil){
-                  version.changeset.each do |key, value|
-                    concat content_tag(:li, "#{value[0]} 改成 #{value[1]}")
-                  end
-                }
-              }
+    concat(custom_helper({state: 'danger',heading: 'Top Secret'}) do
+      post.versions.each_with_index do |version, index|
+        concat content_tag(:tr, nil){
+          concat content_tag(:td, index + 1)
+          concat content_tag(:td, nil){
+            concat content_tag(:ul, nil){
+              version.changeset.each do |key, value|
+                concat content_tag(:li, "#{value[0]} 改成 #{value[1]}")
+              end
             }
-          end
+          }
+          concat content_tag(:td, link_to("復原",  admin.path(:undo, id: version), method: :post, :data => { :confirm => "Are you sure?"}, :class => "btn btn-success"))
         }
-      })
-    }
-
+      end
+    end)
 
   #   row do
   #     col(xs: 6) { datetime_field :updated_at }
@@ -50,11 +45,18 @@ Trestle.resource(:posts) do
   #   end
   end
 
-  # controller do 
-  #   def new
-  #     @value = "aaaa"
-  #   end
-  # end
+  controller do 
+    def undo
+      @version = PaperTrail::Version.find(params[:id])
+      @version.reify.save!
+      flash[:message] = flash_message("published.success", title: "紀錄已復原", message: "") 
+      redirect_to admin.path(:index)
+    end
+  end
+
+  routes do
+    post :undo, on: :member
+  end
 
   # By default, all parameters passed to the update and create actions will be
   # permitted. If you do not have full trust in your users, you should explicitly
--- app/helpers/custom_trestle_helper.rb
+++ app/helpers/custom_trestle_helper.rb
@@ -1,14 +1,10 @@
 module CustomTrestleHelper
-	def order_helper(options)
-	    options = options.reverse_merge({
-	      state: 'default',
-	      heading: nil,
-	      footer: nil
-	    })
-	    content_tag(:div, class: "panel panel-#{options[:state]}") do
-	      concat(content_tag(:div, class: "panel-heading") { options[:heading] }) if options[:heading]
-	      concat(content_tag(:div, class: "panel-body") { yield })
-	      concat(content_tag(:div, class: "panel-footer") { options[:footer] }) if options[:footer]
-	    end
+	def custom_helper(options)
+		content_tag(:div, class: "panel panel-primary") do
+			concat(content_tag(:div, class: "panel-heading") { "編輯紀錄" })
+		    concat(content_tag(:div, class: "panel-body") { 
+		    	concat(content_tag(:table, class: "") { yield })
+		    })
+		end
 	end
 end
